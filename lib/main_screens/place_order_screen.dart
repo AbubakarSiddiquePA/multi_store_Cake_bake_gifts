f694f58750c3fc03b71a91cd3b6a6161440fd09b
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-07-23 20:55:13 +0530

    place order ui

--- lib/main_screens/place_order_screen.dart
+++ lib/main_screens/place_order_screen.dart
@@ -1,3 +1,7 @@
+import 'package:bake_store/widgets/appbar_widgets.dart';
+import 'package:bake_store/widgets/yellow_btn.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PlaceOrderScreen extends StatefulWidget {
@@ -8,10 +12,78 @@ class PlaceOrderScreen extends StatefulWidget {
 }
 
 class _PlaceOrderScreenState extends State<PlaceOrderScreen> {
+  CollectionReference customers =
+      FirebaseFirestore.instance.collection('customers');
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return FutureBuilder<DocumentSnapshot>(
+      future: customers.doc(FirebaseAuth.instance.currentUser!.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return const Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          return Material(
+            color: Colors.grey.shade200,
+            child: SafeArea(
+              child: Scaffold(
+                backgroundColor: Colors.grey.shade200,
+                appBar: AppBar(
+                  elevation: 0,
+                  backgroundColor: Colors.grey.shade200,
+                  leading: const AppBarBackButton(),
+                  title: const AppBarTitle(title: "Place Order"),
+                ),
+                body: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 90,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                bottomSheet: Container(
+                  color: Colors.grey.shade200,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: yellowButtonCstm(
+                        label: "Confirm",
+                        onPressed: () {},
+                        width: 1,
+                        colore: Colors.green),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
