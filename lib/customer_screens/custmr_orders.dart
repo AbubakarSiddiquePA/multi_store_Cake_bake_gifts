dc3c1af38dceb6175fc4fb0b6736ea0b27c47351
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-07-25 22:58:31 +0530

    expansion tile customer order model

--- lib/customer_screens/custmr_orders.dart
+++ lib/customer_screens/custmr_orders.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../models/cust_order_models.dart';
+
 class CustomerOrders extends StatelessWidget {
   const CustomerOrders({super.key});
 
@@ -47,162 +49,8 @@ class CustomerOrders extends StatelessWidget {
           return ListView.builder(
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
-              var order = snapshot.data!.docs[index];
-              return Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.yellow,
-                    ),
-                    borderRadius: BorderRadius.circular(15)),
-                child: ExpansionTile(
-                  title: Container(
-                    constraints: const BoxConstraints(
-                      maxHeight: 80,
-                    ),
-                    width: double.infinity,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 15),
-                          child: Container(
-                            constraints: const BoxConstraints(
-                                maxHeight: 80, maxWidth: 80),
-                            child: Image.network(order["orderimage"]),
-                          ),
-                        ),
-                        Flexible(
-                            child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  order["ordername"],
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.grey.shade600,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                children: [
-                                  Text(("Rs") +
-                                      (order["orderprice"].toStringAsFixed(2))),
-                                  Text(("x") + (order["orderqty"].toString())),
-                                ],
-                              ),
-                            )
-                          ],
-                        ))
-                      ],
-                    ),
-                  ),
-                  subtitle: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "see more...",
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                      Text(order["deliverystatus"]),
-                    ],
-                  ),
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 200,
-                      decoration: BoxDecoration(
-                          color: order["deliverystatus"] == "delivered"
-                              ? Colors.green.shade50
-                              : Colors.yellow.withOpacity(0.2)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              ("Name: ") + (order["custname"]),
-                              style: const TextStyle(fontSize: 15),
-                            ),
-                            Text(
-                              ("Phone No. ") + (order["phone"]),
-                              style: const TextStyle(fontSize: 15),
-                            ),
-                            Text(
-                              ("Email Address: ") + (order["email"]),
-                              style: const TextStyle(fontSize: 15),
-                            ),
-                            Text(
-                              ("Address: ") + (order["address"]),
-                              style: const TextStyle(fontSize: 15),
-                            ),
-                            Row(
-                              children: [
-                                const Text(
-                                  ("Payment Status: "),
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                                Text(
-                                  (order["paymentstatus"]),
-                                  style: const TextStyle(
-                                      color: Colors.orange, fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                const Text(
-                                  ("Delivery Status: "),
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                                Text(
-                                  (order["deliverystatus"]),
-                                  style: const TextStyle(
-                                      color: Colors.green, fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            order["deliverystatus"] == "shipping"
-                                ? Text(
-                                    ("Estimated Delivery Date: ") +
-                                        (order["deliverydate"]),
-                                    style: const TextStyle(fontSize: 15),
-                                  )
-                                : const Text(""),
-                            order["deliverystatus"] == "delivered" &&
-                                    order["orderreview"] == false
-                                ? TextButton(
-                                    onPressed: () {},
-                                    child: const Text("Write a review"))
-                                : const Text(""),
-                            order["deliverystatus"] == "delivered" &&
-                                    order["orderreview"] == true
-                                ? Row(
-                                    children: [
-                                      Icon(
-                                        Icons.check,
-                                        color: Colors.green.shade800,
-                                      ),
-                                      Text(
-                                        "Review added",
-                                        style: TextStyle(
-                                            color: Colors.green.shade800,
-                                            fontStyle: FontStyle.italic),
-                                      )
-                                    ],
-                                  )
-                                : const Text(""),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+              return CustomerOrderModel(
+                order: snapshot.data!.docs[index],
               );
             },
           );
