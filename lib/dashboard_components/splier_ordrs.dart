8391b82542f8d1a366f463e306fd84b14231c05a
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-07-26 12:51:51 +0530

    streaming supplier orders

--- lib/dashboard_components/splier_ordrs.dart
+++ lib/dashboard_components/splier_ordrs.dart
@@ -1,21 +1,36 @@
+import 'package:bake_store/dashboard_components/preparing_orders.dart';
+import 'package:bake_store/dashboard_components/shipping_orders.dart';
 import 'package:bake_store/widgets/appbar_widgets.dart';
 import 'package:flutter/material.dart';
+
+import 'delivered_orders.dart';
 
 class SupllierOrders extends StatelessWidget {
   const SupllierOrders({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const AppBarTitle(title: "Supllier Orders"),
-        leading: const AppBarBackButton(),
-        bottom: const TabBar(tabs: [
-          RepeatedTab(label: "Preparing"),
-          RepeatedTab(label: "Shipping"),
-          RepeatedTab(label: "Delivered")
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: const AppBarBackButton(),
+          title: const AppBarTitle(title: "Supllier Orders"),
+          bottom: TabBar(
+              indicatorWeight: 8,
+              indicatorColor: Colors.grey.shade900,
+              tabs: const [
+                RepeatedTab(label: "Preparing"),
+                RepeatedTab(label: "Shipping"),
+                RepeatedTab(label: "Delivered")
+              ]),
+        ),
+        body: const TabBarView(children: [
+          Preparing(),
+          Shipping(),
+          Delivered(),
         ]),
       ),
     );
@@ -32,7 +47,7 @@ class RepeatedTab extends StatelessWidget {
       child: Center(
         child: Text(
           label,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.grey,
           ),
         ),
---
+++ lib/models/suplir_order_model.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class SupplierOrderModel extends StatelessWidget {
+  final dynamic order;
+  const SupplierOrderModel({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.yellow,
+            ),
+            borderRadius: BorderRadius.circular(15)),
+        child: ExpansionTile(
+          title: Container(
+            constraints: const BoxConstraints(
+              maxHeight: 80,
+            ),
+            width: double.infinity,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: Container(
+                    constraints:
+                        const BoxConstraints(maxHeight: 80, maxWidth: 80),
+                    child: Image.network(order["orderimage"]),
+                  ),
+                ),
+                Flexible(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          order["ordername"],
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey.shade600,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(("Rs") +
+                              (order["orderprice"].toStringAsFixed(2))),
+                          Text(("x") + (order["orderqty"].toString())),
+                        ],
+                      ),
+                    )
+                  ],
+                ))
+              ],
+            ),
+          ),
+          subtitle: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "see more...",
+                style: TextStyle(color: Colors.blue),
+              ),
+              Text(order["deliverystatus"]),
+            ],
+          ),
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(color: Colors.yellow.withOpacity(0.2)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      ("Name: ") + (order["custname"]),
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      ("Phone No. ") + (order["phone"]),
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      ("Email Address: ") + (order["email"]),
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      ("Address: ") + (order["address"]),
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    Row(
+                      children: [
+                        const Text(
+                          ("Payment Status: "),
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          (order["paymentstatus"]),
+                          style: const TextStyle(
+                              color: Colors.orange, fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text(
+                          ("Delivery Status: "),
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          (order["deliverystatus"]),
+                          style: const TextStyle(
+                              color: Colors.green, fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text(
+                          ("Order Date: "),
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          (DateFormat("yyyy-MM-dd")
+                              .format(order["orderdate"].toDate())
+                              .toString()),
+                          style: const TextStyle(
+                              color: Colors.green, fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    order["deliverystatus"] == "delivered"
+                        ? const Text(
+                            "This order has been delivered to you \n Kepp shopping with us",
+                            style: TextStyle(color: Colors.green),
+                          )
+                        : Row(
+                            children: [
+                              const Text(
+                                ("Change Delivery Status To: "),
+                                style: TextStyle(fontSize: 15),
+                              ),
+                              order["deliverystatus"] == "preparing"
+                                  ? TextButton(
+                                      onPressed: () {},
+                                      child: const Text("Shipping? "))
+                                  : TextButton(
+                                      onPressed: () {},
+                                      child: const Text("Delivered "))
+                            ],
+                          ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
