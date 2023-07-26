2d81640283378da575fc18786a5fca731238ba48
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-07-26 13:52:07 +0530

    stream data in  manage products screen

--- lib/dashboard_components/manage_products.dart
+++ lib/dashboard_components/manage_products.dart
@@ -1,17 +1,68 @@
 import 'package:bake_store/widgets/appbar_widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+
+import '../models/product_model.dart';
 
 class ManageProduct extends StatelessWidget {
   const ManageProduct({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _productsStream = FirebaseFirestore.instance
+        .collection('products')
+        .where("sid", isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
         title: const AppBarTitle(title: "Manage Product"),
         leading: const AppBarBackButton(),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _productsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text(
+                "this category has no items yet",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: "Acme",
+                    letterSpacing: 1.5,
+                    fontSize: 24,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold),
+              ),
+            );
+          }
+          return SingleChildScrollView(
+            child: StaggeredGridView.countBuilder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: snapshot.data!.docs.length,
+                crossAxisCount: 2,
+                itemBuilder: (context, index) {
+                  return ProductModel(
+                    products: snapshot.data!.docs[index],
+                  );
+                },
+                staggeredTileBuilder: (context) => const StaggeredTile.fit(1)),
+          );
+        },
       ),
     );
   }
