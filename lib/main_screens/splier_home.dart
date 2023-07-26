87175b1384d566946151875d83be9d683325ff7a
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-07-26 14:18:56 +0530

    badge added in supplier dashboard screen to notify supplier that order has recieved from the user

--- lib/main_screens/splier_home.dart
+++ lib/main_screens/splier_home.dart
@@ -3,7 +3,10 @@ import 'package:bake_store/main_screens/dashboard.dart';
 import 'package:bake_store/main_screens/home.dart';
 import 'package:bake_store/main_screens/stores.dart';
 import 'package:bake_store/main_screens/upload_product.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart ' as badges;
 
 class SupplierHomeScreen extends StatefulWidget {
   const SupplierHomeScreen({super.key});
@@ -27,29 +30,58 @@ class _SupplierHomeScreenState extends State<SupplierHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _tabs[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        // elevation: 0,
-        type: BottomNavigationBarType.fixed,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-        selectedItemColor: Colors.black,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.category), label: "Category"),
-          BottomNavigationBarItem(icon: Icon(Icons.store), label: "Stores"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard), label: "Dashboard"),
-          BottomNavigationBarItem(icon: Icon(Icons.upload), label: "Upload"),
-        ],
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection("orders")
+          .where("sid", isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .where("deliverystatus", isEqualTo: "preparing")
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Material(
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        return Scaffold(
+          body: _tabs[_selectedIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            // elevation: 0,
+            type: BottomNavigationBarType.fixed,
+            selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+            selectedItemColor: Colors.black,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.category), label: "Category"),
+              BottomNavigationBarItem(icon: Icon(Icons.store), label: "Stores"),
+              BottomNavigationBarItem(
+                  icon: badges.Badge(
+                      showBadge: snapshot.data!.docs.isEmpty ? false : true,
+                      // stackFit: StackFit.expand,
+                      badgeStyle:
+                          const badges.BadgeStyle(badgeColor: Colors.yellow),
+                      badgeContent: Text(
+                        snapshot.data!.docs.length.toString(),
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      child: Icon(Icons.dashboard)),
+                  label: "Dashboard"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.upload), label: "Upload"),
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
