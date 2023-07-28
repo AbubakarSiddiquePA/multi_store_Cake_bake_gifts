81cca83079aa2c2de12c1124440ee3112d60afba
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-07-28 11:51:04 +0530

    streaming data in search screen

--- lib/minor_screens/search.dart
+++ lib/minor_screens/search.dart
@@ -1,24 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  String searchInput = "";
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back),
-          color: Colors.black,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+            color: Colors.black,
+          ),
+          title: CupertinoSearchTextField(
+            onChanged: (value) {
+              setState(() {
+                searchInput = value;
+              });
+            },
+          ),
         ),
-        title: const CupertinoSearchTextField(),
-      ),
-    );
+        body: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection("products").snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Material(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+
+              final result = snapshot.data!.docs.where((element) =>
+                  element["proname".toLowerCase()]
+                      .contains(searchInput.toLowerCase()));
+
+              return ListView(
+                  children: result.map((e) => Text(e["proname"])).toList());
+            }));
   }
 }
