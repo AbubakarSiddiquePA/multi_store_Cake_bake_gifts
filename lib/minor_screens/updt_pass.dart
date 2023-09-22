1e3b99319d23ff15eee88bc4887b7f64ed914a7c
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-09-22 14:51:36 +0530

    change password password validation package added

--- lib/minor_screens/updt_pass.dart
+++ lib/minor_screens/updt_pass.dart
@@ -2,8 +2,8 @@ import 'package:bake_store/providers/auth_repo.dart';
 import 'package:bake_store/widgets/appbar_widgets.dart';
 import 'package:bake_store/widgets/yellow_btn.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_pw_validator/flutter_pw_validator.dart';
 
 class UpdatePassword extends StatefulWidget {
   const UpdatePassword({super.key});
@@ -15,6 +15,8 @@ class UpdatePassword extends StatefulWidget {
 class _UpdatePasswordState extends State<UpdatePassword> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final TextEditingController oldPasswordController = TextEditingController();
+  final TextEditingController newPasswordController = TextEditingController();
+
   bool checkPassword = true;
   @override
   Widget build(BuildContext context) {
@@ -27,84 +29,109 @@ class _UpdatePasswordState extends State<UpdatePassword> {
         leading: const AppBarBackButton(),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 50, 10, 30),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                const Text(
-                  "To change your password please fill the form below and click save",
-                  style: TextStyle(
-                      fontSize: 20,
-                      letterSpacing: 1.1,
-                      color: Colors.blueGrey,
-                      fontFamily: "Acme",
-                      fontStyle: FontStyle.italic),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "enter your password";
-                      }
-                      return null;
-                    },
-                    controller: oldPasswordController,
-                    decoration: passwordFormDecoration.copyWith(
-                      labelText: "Old Password",
-                      hintText: "Enter your current password",
-                      errorText:
-                          checkPassword != true ? "not valid password" : null,
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: 800,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 50, 10, 30),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    const Text(
+                      "To change your password please fill the form below and click save",
+                      style: TextStyle(
+                          fontSize: 20,
+                          letterSpacing: 1.1,
+                          color: Colors.blueGrey,
+                          fontFamily: "Acme",
+                          fontStyle: FontStyle.italic),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: passwordFormDecoration.copyWith(
-                      labelText: "New Password",
-                      hintText: "Enter your New password",
+                    const SizedBox(
+                      height: 30,
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: passwordFormDecoration.copyWith(
-                      labelText: "Repeat Password",
-                      hintText: "Re-Enter your New password",
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "enter your password";
+                          }
+                          return null;
+                        },
+                        controller: oldPasswordController,
+                        decoration: passwordFormDecoration.copyWith(
+                          labelText: "Old Password",
+                          hintText: "Enter your current password",
+                          errorText: checkPassword != true
+                              ? "not valid password"
+                              : null,
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: newPasswordController,
+                        decoration: passwordFormDecoration.copyWith(
+                          labelText: "New Password",
+                          hintText: "Enter your New password",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value != newPasswordController.text) {
+                            return "Password not matching";
+                          }
+                          return null;
+                        },
+                        decoration: passwordFormDecoration.copyWith(
+                          labelText: "Repeat Password",
+                          hintText: "Re-Enter your New password",
+                        ),
+                      ),
+                    ),
+                    FlutterPwValidator(
+                      controller: newPasswordController,
+                      minLength: 8,
+                      uppercaseCharCount: 1,
+                      numericCharCount: 2,
+                      specialCharCount: 1,
+                      width: 400,
+                      height: 200,
+                      onSuccess: () {},
+                      onFail: () {},
+                    ),
+                    const Spacer(),
+                    yellowButtonCstm(
+                        label: "Save Changes",
+                        onPressed: () async {
+                          print(FirebaseAuth.instance.currentUser!);
+                          if (formKey.currentState!.validate()) {
+                            checkPassword = await AuthRepo.CheckOldPassword(
+                                FirebaseAuth.instance.currentUser!.email!,
+                                oldPasswordController.text);
+                            setState(() {
+                              // checkPassword = false;
+                            });
+                            checkPassword == true
+                                ? print("valid old password")
+                                : print("not valid old password");
+                            // ignore: avoid_print
+                            print("form valid");
+                          } else {
+                            // ignore: avoid_print
+                            print("form not valid");
+                          }
+                        },
+                        width: 0.7,
+                        colore: Colors.green)
+                  ],
                 ),
-                const Spacer(),
-                yellowButtonCstm(
-                    label: "Save Changes",
-                    onPressed: () async {
-                      if (formKey.currentState!.validate()) {
-                        checkPassword = await AuthRepo.CheckOldPassword(
-                            FirebaseAuth.instance.currentUser!.email!,
-                            oldPasswordController.text);
-                        setState(() {
-                          // checkPassword = false;
-                        });
-                        checkPassword == true
-                            ? print("valid old password")
-                            : print("not valid old password");
-                        // ignore: avoid_print
-                        print("form valid");
-                      } else {
-                        // ignore: avoid_print
-                        print("form not valid");
-                      }
-                    },
-                    width: 0.7,
-                    colore: Colors.green)
-              ],
+              ),
             ),
           ),
         ),
